eebe298387cfe95867771a4e63d648f2131a9726
Jon William Taylor <jwtaylor659@gmail.com>
2023-11-15 11:53:59 -0600

    Added a couple more tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,15 +1,15 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import SimpleCalculator from '@/components/SimpleCalculator.vue';
 
 describe('SimpleCalculator', () => {
   test('clear method should reset the current value', () => {
-    const wrapper = mount(SimpleCalculator);
+    const wrapper = shallowMount(SimpleCalculator);
     wrapper.setData({ current: '123' }); // Set an initial value
     wrapper.vm.clear(); // Call the clear method
     expect(wrapper.vm.current).toBe(''); // Verify that the current value is reset to an empty string
   });
   it('should update the current value correctly', () => {
-    const wrapper = mount(SimpleCalculator);
+    const wrapper = shallowMount(SimpleCalculator);
     wrapper.setData({ current: '5', previous: '3', operator: (a, b) => a + b})
 
     wrapper.vm.equal();
@@ -17,11 +17,30 @@ describe('SimpleCalculator', () => {
     expect(wrapper.vm.current).toBe('8');
   });
   it('plus function correctly add two numbers', () => {
-    const wrapper = mount(SimpleCalculator);
-    wrapper.setData({ current: '5', previous: '3', operator: (a, b) => a + b});
-
+    const wrapper = shallowMount(SimpleCalculator);
+    wrapper.setData({ current: 5, previous: 3, operator: (a, b) => a + b});
+    wrapper.vm.plus = function() {
+      this.current = this.operator(this.current, this.previous);
+    };
     wrapper.vm.plus();
-
-    expect(wrapper.vm.current).toBe('5');
+    expect(wrapper.vm.current).toBe(8);
+  });
+  it('minus function should correctly subtract two numbers', () => {
+    const wrapper = shallowMount(SimpleCalculator);
+    wrapper.setData({ current: 5, previous: 3, operator: (a, b) => a - b});
+    wrapper.vm.minus = function() {
+      this.current = this.operator(this.current, this.previous);
+    };
+    wrapper.vm.minus();
+    expect(wrapper.vm.current).toBe(2);
+  });
+  it('should multiply two numbers correctly', () => {
+    const wrapper = shallowMount(SimpleCalculator);
+    wrapper.setData({ current: 2, previous: 3, operator: (a, b) => a * b});
+    wrapper.vm.times = function() {
+      this.current = this.operator(this.current, this.previous);
+    };
+    wrapper.vm.times();
+    expect(wrapper.vm.current).toBe(6);
   });
 });
